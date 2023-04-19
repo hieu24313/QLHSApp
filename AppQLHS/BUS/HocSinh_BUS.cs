ee33b58b6274d989fdc6ad8841a920f201371bcd
hieu24313 <2051050138hieu@ou.edu.com>
2023-04-19 13:06:42 +0700

    update

---
+++ AppQLHS/BUS/HocSinh_BUS.cs
@@ -0,0 +1,79 @@
+﻿using DAL;
+using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS
+{
+    public class HocSinh_BUS
+    {
+        public List<HocSinh_DTO> getHocSinh()
+        {
+            HocSinh_DAL hs = new HocSinh_DAL();
+            return hs.getHocSinh();
+        }
+
+        public List<HocSinh_DTO> getHocSinhbyID(int id)
+        {
+            HocSinh_DAL hs = new HocSinh_DAL();
+            return hs.getHocSinhByID(id);
+        }
+
+        public List<HocSinh_DTO> getHocSinhbyLopID(int id)
+        {
+            HocSinh_DAL hs = new HocSinh_DAL();
+            return hs.getHocSinhbyLopID(id);
+        }
+
+        public List<HocSinh_DTO> getHocSinhbyTen(string ten)
+        {
+            HocSinh_DAL hs = new HocSinh_DAL();
+            return hs.getHocSinhByTen(ten);
+        }
+
+        public bool addHocSinh(HocSinh_DTO hocSinh)
+        {
+            HocSinh_DAL hs = new HocSinh_DAL();
+            bool kt = hs.addHocSinh(hocSinh);
+            if (kt)
+            {
+                return true;
+            }
+            else 
+            { 
+                return false; 
+            }
+        }
+
+        public bool updateHocSinh(HocSinh_DTO hs)
+        {
+            HocSinh_DAL h = new HocSinh_DAL();
+            bool kt = h.updateHocSinh(hs);
+            if (kt)
+            {
+                return true;
+            }
+            else 
+            {
+                return false; 
+            }
+        }
+
+        public bool delHocSinh(int id)
+        {
+            HocSinh_DAL h = new HocSinh_DAL();
+            bool kt = h.delHocSinh(id);
+            if (kt)
+            {
+                return true;
+            }
+            else 
+            { 
+                return false; 
+            }
+        }
+    }
+}
---
+++ AppQLHS/BUS/LoaiDiem_BUS.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using DTO;
+
+namespace BUS
+{
+    public class LoaiDiem_BUS
+    {
+        LoaiDiem_DAL ld = new LoaiDiem_DAL();
+        public List<LoaiDiem_DTO> getLoaiDiem()
+        {
+            return ld.getLoaiDiem();
+        }
+        public List<LoaiDiem_DTO> getLoaiDiemid(int id)
+        {
+            return ld.getLoaiDiemid(id);
+        }
+        public List<LoaiDiem_DTO> getLoaiDiemten(string ten)
+        {
+            return ld.getLoaiDiemten(ten);
+        }
+
+        public bool addLoaiDiem(LoaiDiem_DTO d) 
+        {
+            bool kt = ld.addLoaiDiem(d);
+            if (kt)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool delLoaiDiem(int id)
+        {
+            if (ld.delLoaiDiem(id))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool updateLoaiDiem(LoaiDiem_DTO loaidiem)
+        {
+            LoaiDiem_DAL ld = new LoaiDiem_DAL();
+            bool kt = ld.updateLoaiDiem(loaidiem);
+            if (kt)
+            {
+                return true;
+            }
+            else 
+            { 
+                return false;
+            }
+        }
+    }
+}
